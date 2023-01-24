9a0b341acd22fe764221efe3804be11640fdb118
elbroandrew <elbroandrew@gmail.com>
2023-01-24 15:30:06 +1000

    concurrent coprogram, non buff channel

--- main.go
+++ main.go
@@ -1,37 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"math"
-	"strings"
-)
+import "fmt"
 
 func main() {
+	ch := make(chan string)
 
-	fmt.Println(math.MinInt8)
-}
+	go func() {
+		message := <-ch
+		fmt.Println(message)
+		ch <- "pong"
+	}()
 
-func isPalindrome(s string) bool {
-
-	var skips = ",.!?_-:;' "
-	var newString = ""
-	for i := 0; i < len(s); i++ {
-		if !strings.ContainsRune(skips, rune(s[i])) {
-			newString += string(s[i])
-		}
-	}
-	newString = strings.ToLower(newString)
-
-	var p1 = 0
-	var p2 = len(newString) - 1
-
-	for p1 < p2 {
-		if newString[p1] == newString[p2] {
-			p1++
-			p2--
-		} else {
-			return false
-		}
-	}
-	return true
+	ch <- "ping"
+	fmt.Println(<-ch)
 }
