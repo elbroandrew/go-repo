a4e59a46e75fd903e31a52488000870e37e7c75b
Andrew <elbroandrew@gmail.com>
2023-01-31 16:34:29 +1000

    sliding window

--- main.go
+++ main.go
@@ -3,14 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	ch := make(chan string)
+	var a = []int{2, 3, 4, 4, 3, 1, 4, 5, 4, 7, 6}
+	var x = maxSubArray(a, 3)
+	fmt.Println(x)
+}
 
-	go func() {
-		message := <-ch
-		fmt.Println(message)
-		ch <- "pong"
-	}()
+// sliding window
+func maxSubArray(arr []int, num int) any {
+	var maxSum = 0
+	var tempSum = 0
+	if len(arr) < num {
+		return nil
+	}
+	//find sum of 3 elements
+	for i := 0; i < num; i++ {
+		maxSum += arr[i]
+	}
+	tempSum = maxSum
+	// 1,2,3,4,5 -> -arr[0] 2+3+4 +arr[i];
+	for i := num; i < len(arr); i++ {
+		tempSum = -arr[i-num] + tempSum + arr[i]
+		if maxSum < tempSum {
+			maxSum = tempSum
+		}
+	}
 
-	ch <- "ping"
-	fmt.Println(<-ch)
+	return maxSum
 }
