195fe11c888e34f0d27d744fdd20ba4d27fb6b98
andrew <elbroandrew@gmail.com>
2023-08-25 23:42:22 +1000

    add func for ozon

--- main.go
+++ main.go
@@ -12,26 +12,25 @@ func Add(x int, y int) int {
 
 func main() {
 
-	file, err := os.Open("data1.txt")
-	if err != nil {
-		fmt.Println("Unable to open file:", err)
-		return
-	}
+	// file, err := os.Open("data1.txt")
+	// if err != nil {
+	// 	fmt.Println("Unable to open file:", err)
+	// 	return
+	// }
 
-	defer file.Close()
+	// defer file.Close()
 
-	reader := bufio.NewReader(file)
+	reader := bufio.NewReader(os.Stdin)
 	writer := bufio.NewWriter(os.Stdout)
 
 	var testCount int
 	fmt.Fscan(reader, &testCount)
-	// fmt.Printf("%d\n", testCount)
 
 	for i := 0; i < testCount; i++ {
 		var n, m int
 		fmt.Fscan(reader, &n, &m)
 		fmt.Fprintln(writer, Add(n, m))
-		
+
 	}
 
 	writer.Flush()
