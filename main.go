8c4d0e8df1c5ced92697f8ecb7e3b73bba3200c2
andrew <elbroandrew@gmail.com>
2023-08-25 15:16:08 +1000

    overwrite existing file

--- main.go
+++ main.go
@@ -11,24 +11,28 @@ func Add(x int, y int) int {
 }
 
 func main() {
-	rows := []string{
-		"Hello Go!",
-		"Welcome to Golang",
-	}
 
-	file, err := os.Create("file.txt")
-	writer := bufio.NewWriter(file)
+	// file, err := os.Open("file.txt") // open in read-only format
+	file, err := os.OpenFile("file.txt", os.O_RDWR|os.O_TRUNC, 0755)
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		fmt.Println("Unable to open file:", err)
+		return
 	}
 
 	defer file.Close()
 
-	for _, row := range rows {
-		writer.WriteString(row)  // запись строки
-		writer.WriteString("\n") // перенос строки
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(file)
+
+	var n string
+	fmt.Fscan(reader, &n)
+	// fmt.Fprintln(writer, n)
+	_, err = writer.WriteString(n)
+	if err != nil {
+		fmt.Println("Unable to write file:", err)
 	}
+	fmt.Printf("wrote %s\n", n)
+
 	writer.Flush()
 
 }
