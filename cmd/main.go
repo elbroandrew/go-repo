519d89599d76bd82a6d5b51631ff78e5c999792d
Andrew <elbroandrew@gmail.com>
2024-02-22 13:27:12 +1000

    sum func

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,17 @@
-package cmd
+package main
+
+import "fmt"
+
+func Sum(x ...int) (res int) {
+	for _, v := range x {
+		res += v
+	}
+	return
+}
+
+func main() {
+
+	sum := Sum(1, 2, 3, 4, 5)
+	fmt.Println(sum)
+
+}
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/elbroandrew/go-repo
 
 go 1.20
-
-require github.com/gorilla/mux v1.8.1
