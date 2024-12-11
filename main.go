13c53de0e2a50fcace43ae68ea4b193881f08bf8
Andrew <elbroandrew@gmail.com>
2024-12-11 20:31:06 +1000

    Mutex

--- main.go
+++ main.go
@@ -2,43 +2,31 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
-	"net/http"
+	"sync"
 )
 
-func main() {
-	router := gin.Default()
-	router.LoadHTMLGlob("templates/*")
-	router.GET("/index", imageLoadHandler)
-	if err := router.Run(); err != nil {
-		panic(err)
-	}
-}
+const num = 500
 
-/*
-	func(c *gin.Context) {
-			c.HTML(http.StatusOK, "index.tmpl", gin.H{
-				"title": "Main website",
-			})
-		}
-*/
-func imageLoadHandler(c *gin.Context) {
-	//content := "Download file"
-	//fileName := "hello.txt"
-	//c.Header("Content-Disposition", "attachment; filename="+fileName)
-	//c.Header("Content-Type", "application/text/plain")
-	//c.Header("Accept-Length", fmt.Sprintf("%d", len(content)))
-	//_, err := c.Writer.Write([]byte(content))
-	//if err != nil {
-	//	return
-	//}
-	c.HTML(http.StatusOK, "index.tmpl", gin.H{
-		"title": "Main website",
-	}) //невозможно отрендерирть хтмл темплейт и отправить файл, надло или то, или то сделать.
-	fileName := "hello.txt"
-	content := "Download file"
-	c.Header("Content-Disposition", "attachment; filename="+fileName)
-	c.Header("Content-Type", "application/text/plain")
-	c.Header("Accept-Length", fmt.Sprintf("%d", len(content)))
-	c.Writer.Write([]byte(content))
+
+func main() {
+
+	mutex := sync.Mutex{}
+	wg := sync.WaitGroup{}
+	wg.Add(num)
+
+	value := 0
+	for i := 0; i < num; i++ {
+		go func(){
+			defer wg.Done()
+
+			mutex.Lock()
+			value++
+			mutex.Unlock()
+		}()
+	}
+
+	wg.Wait()
+
+	fmt.Println(value)
+
 }
