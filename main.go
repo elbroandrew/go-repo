3007bcce55d4bc5e9e441cad69ceea8e1f4ef3be
Andrew <elbroandrew@gmail.com>
2025-01-15 16:20:27 +1000

    dynamic programming: grashopper

--- main.go
+++ main.go
@@ -1,31 +1,37 @@
 package main
 
+/*
+DYNAMIC PROGRAMMING
+
+Grashopper
+
+1 2 3 4 5 6 7 ... n
+A grashopper can jump either 1 or 2 cells ahead,
+How many ways (K) are there to reach the N-th cell ?
+
+Kn = Kn-1 + Kn-2
+*/
 import (
 	"fmt"
-	"sync"
 )
 
-const num = 500
+func numberOfTrajectories(n int) int{
+	var K = make([]int, n+1)
+	K[0] = 0
+	K[1] = 1
+	for i := 2; i <= n; i++ {
+		K[i] = K[i-1] + K[i-2]
+	}
+	return K[n]
+}
 
 func main() {
 
-	mutex := sync.Mutex{}
-	wg := sync.WaitGroup{}
-	wg.Add(num)
-
-	value := 0
-	for i := 0; i < num; i++ {
-		go func() {
-			defer wg.Done()
-
-			mutex.Lock()
-			value++
-			mutex.Unlock()
-		}()
-	}
-
-	wg.Wait()
-
-	fmt.Println(value)
+	var finish int;
+	fmt.Scan(&finish)
+	fmt.Printf("Grashopper has %d trajectories from 1 to %d\n", numberOfTrajectories(finish),  finish)
+	
+	//5
+    //Grashopper has 5 trajectories from 1 to 5 
 
 }
